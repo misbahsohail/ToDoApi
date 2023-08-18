d7ab8914554e82f68f4f983b8f8234eabdf6759d
Misbah Sohail <msbh.sohail@gmail.com>
2023-08-19 00:47:45 +0500

    Updated Fetching jwt token from config instead of defining in main code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
+const env = process.env.NODE_ENV || "development";
+const configPath = `${__dirname}/../config/config.json`;
+const config = require(configPath)[env];
 
 module.exports.requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
   // check if token exists and is correct
   if (token) {
-    jwt.verify(token, "my sectret", (err, decodedToken) => {
+    jwt.verify(token, config.jwt_token, (err, decodedToken) => {
       if (err) {
         res.status(400).send(err.message);
       } else {
@@ -14,6 +17,6 @@ module.exports.requireAuth = (req, res, next) => {
       }
     });
   } else {
-    res.status(400).send("User unauthenticated");
+    res.status(400).send("User Unauthenticated");
   }
 };
