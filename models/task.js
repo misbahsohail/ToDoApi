8812f5ed7d7517a8c6db76a52a7e99be5430c2c8
Misbah Sohail <msbh.sohail@gmail.com>
2023-08-18 23:43:18 +0500

    Made sequelize instances singleton

--- models/task.js
+++ models/task.js
@@ -1,36 +1,26 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("todoapp", "root", "1234", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-const Task = sequelize.define("tasks", {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-  },
-  body: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-  },
-  user: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-  },
-  isCompleted: {
-    type: DataTypes.BOOLEAN,
-    required: true,
-  },
-});
-
-// sequelize.sync().then(() => {
-//     console.log('Task table created successfully!');
-//  }).catch((error) => {
-//     console.error('Unable to create table : ', error);
-//  });
-
-module.exports = Task;
+module.exports = (sequelize, Sequelize) => {
+  const Task = sequelize.define("Task", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    isCompleted: {
+      type: DataTypes.BOOLEAN,
+      required: true,
+    },
+  });
+  return Task;
+};
--- models/user.js
+++ models/user.js
@@ -1,61 +1,52 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 
-const sequelize = new Sequelize("todoapp", "root", "1234", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-const User = sequelize.define("users", {
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-    unique: true,
-    validate: {
-      isEmail: true,
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-    validate: {
-      len: [6, 50],
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      validate: {
+        len: [6, 50],
+      },
     },
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    required: true,
-  },
-});
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+  });
+  User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    console.log("User created", user);
+  });
 
-// sequelize.sync().then(() => {
-//     console.log('Task table created successfully!');
-//  }).catch((error) => {
-//     console.error('Unable to create table : ', error);
-//  });
+  // static method to login user
 
-User.beforeCreate(async (user) => {
-  const salt = await bcrypt.genSalt();
-  user.password = await bcrypt.hash(user.password, salt);
-  console.log("User created", user);
-});
+  User.login = async (email, password) => {
+    const user = await User.findOne({ where: { email } });
 
-// static method to login user
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
 
-User.login = async (email, password) => {
-  const user = await User.findOne({ where: { email } });
-
-  if (user) {
-    const auth = await bcrypt.compare(password, user.password);
-    if (auth) {
-      return user;
+      throw Error("incorrect password");
     }
+    throw Error("incorrect email");
+  };
 
-    throw new Error("incorrect password debug :: ");
-  }
-  throw Error("incorrect email");
+  return User;
 };
-module.exports = User;
